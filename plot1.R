2a17cdd4bfe100515c6bce81d6aa4d2a744502ee
YuanhangWang <YuanhangWang_Silvio@hotmail.com>
2014-05-06 23:52:45 +0800

    Implemented plot2

--- plot1.R
+++ plot1.R
@@ -1,13 +1,15 @@
 ## A function used to draw a histgraph of global active power
-plot1 <- function(file.dir = "household_power_consumption.txt") {
+plot1 <- function(file.dir = "household_power_consumption.txt", data = NULL) {
   print("-->Retrieving data...")
   ## Read data
-  data <- read.table(file.dir, na.strings = "?", sep = ";", header = TRUE,
-                     colClasses = c(rep("character", times = 2),
-                                    rep("numeric", times = 7)))
-  ## Transform and filter data
-  data <- transform(data, Date = as.Date(Date, format = "%d/%m/%Y"))
-  data <- subset(data, Date %in% c(as.Date("2007-02-01"), as.Date("2007-02-02")))
+  if(is.null(data)) {
+    data <- read.table(file.dir, na.strings = "?", sep = ";", header = TRUE,
+                       colClasses = c(rep("character", times = 2),
+                                      rep("numeric", times = 7)))
+    ## Transform and filter data
+    data <- transform(data, Date = as.Date(Date, format = "%d/%m/%Y"))
+    data <- subset(data, Date %in% c(as.Date("2007-02-01"), as.Date("2007-02-02")))
+  }
   print("-->Drawing graph...")
   ## draw histgraph
   png("plot1.png", width = 480, height= 480)
